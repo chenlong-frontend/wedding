d8174acd0c6ab042d1c5d9c4f5a3b519b5eac742
chenlong-frontend <2956957982@qq.com>
2022-08-21 14:41:17 +0800

    地图

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ map.add(marker);//添加到地图
 
 wx.config({
     debug: true, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
-    appId: '', // 必填，公众号的唯一标识
+    appId: 'wx20f168f01ea432c5', // 必填，公众号的唯一标识
     timestamp: '', // 必填，生成签名的时间戳
     nonceStr: '', // 必填，生成签名的随机串
     signature: '',// 必填，签名
