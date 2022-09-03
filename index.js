a031eb91829deb93d940c828404279199416fa9c
chenlong-frontend <2956957982@qq.com>
2022-09-03 22:02:02 +0800

    添加地图导航

--- index.js
+++ index.js
@@ -17,6 +17,10 @@ $('#audio').on('click', function() {
     }
 })
 
+$('#map').on('click', function() {
+    location.href = 'https://m.amap.com/search/mapview/__p=B0FFJ2LG6E,32.972239190607304,120.38758277893069,%E7%9B%90%E5%9F%8E%E6%A2%A6%E5%B9%BB%E8%BF%B7%E5%AE%AB%E5%BA%A6%E5%81%87%E9%85%92%E5%BA%97,%E5%A4%A7%E4%B8%B0%E5%8C%BA%E8%8D%89%E5%A0%B0%E9%95%87%E4%B8%89%E5%85%83%E6%9D%91%E4%B8%83%E7%BB%84&src=app_share&callnative=1&callapp=0&userRelationToken=9dfc941d2b8f11ed94f000163e0d1dc11&poiid=B0FFJ2LG6E'
+})
+
 var map = new AMap.Map('amap', {
     zoom: 14,//级别
     center: [120.387583, 32.972239],//中心点坐标
@@ -27,6 +31,7 @@ var marker = new AMap.Marker({
     position:[120.387583, 32.972239]//位置
 })
 map.add(marker);//添加到地图
+
 
 
 // wx.config({
